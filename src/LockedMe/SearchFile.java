7e9bdc4beebddfc1dd7fd80302540426ffb20733
pranjaltalukdar <pranjaltalukdar@ip-172-31-8-167.ec2.internal>
2021-01-24 12:38:59 +0000

    Complete Project

--- src/LockedMe/SearchFile.java
+++ src/LockedMe/SearchFile.java
@@ -8,7 +8,7 @@ public class SearchFile {
 	public static void Search()  
     { 
 		Scanner scanner = new Scanner( System.in );
-		 System.out.println("\nEnter the file path:\n");
+		 System.out.println("\nEnter the file path(For eg:/home/pranjaltalukdar/Desktop/Project_testing/):\n");
 		 String dirPath = scanner.nextLine();
 		 File folder = new File(dirPath);
         String[] flist = folder.list(); 
